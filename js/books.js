ab702901c49ee4c44965ba7163ac9ffbee7b1f5e
losdelgimpos <joetinston@gmail.com>
2015-09-13 22:49:20 +0100

    restructure and collections

---
+++ js/books.js
@@ -0,0 +1,78 @@
+console.log('books script loaded');
+
+$(document).ready(function(){
+		
+		$('#book1').on('click',function(){
+		$('#book1details').removeClass('hide');
+		$('#book1details').siblings().addClass('hide');
+		})
+
+		$('#book2').on('click',function(){
+		$('#book2details').removeClass('hide');
+		$('#book2details').siblings().addClass('hide');
+		})
+
+		$('#book3').on('click',function(){
+		$('#book3details').removeClass('hide');
+		$('#book3details').siblings().addClass('hide');
+		})				
+
+		$('#book4').on('click',function(){
+		$('#book4details').removeClass('hide');
+		$('#book4details').siblings().addClass('hide');
+		})
+
+		$('#book5').on('click',function(){
+		$('#book5details').removeClass('hide');
+		$('#book5details').siblings().addClass('hide');
+		})		
+
+		$('#book6').on('click',function(){
+		$('#book6details').removeClass('hide');
+		$('#book6details').siblings().addClass('hide');
+		})	
+
+		$('#book7').on('click',function(){
+		$('#book7details').removeClass('hide');
+		$('#book7details').siblings().addClass('hide');
+		})	
+
+		$('#book7').on('click',function(){
+		$('#book7details').removeClass('hide');
+		$('#book7details').siblings().addClass('hide');
+		})	
+
+		$('#book8').on('click',function(){
+		$('#book8details').removeClass('hide');
+		$('#book8details').siblings().addClass('hide');
+		})	
+
+		$('#book9').on('click',function(){
+		$('#book9details').removeClass('hide');
+		$('#book9details').siblings().addClass('hide');
+		})	
+
+		$('#book10').on('click',function(){
+		$('#book10details').removeClass('hide');
+		$('#book10details').siblings().addClass('hide');
+		})	
+
+		$('#book11').on('click',function(){
+		$('#book11details').removeClass('hide');
+		$('#book11details').siblings().addClass('hide');
+		})	
+
+		$('#book12').on('click',function(){
+		$('#book12details').removeClass('hide');
+		$('#book12details').siblings().addClass('hide');
+		})	
+
+		$('.book-row').on('click',function(){		
+		$('#bookContainer').slideDown();
+		})
+
+
+	
+
+})
+
